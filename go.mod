e987d419da754f94804ebfb33c6281e4092a28dc
cmatri <connorperkins@cox.net>
2020-05-14 23:08:01 -0400

    Basic tui and communication

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module com/cmatri/irc_client
 
 go 1.14
+
+require github.com/marcusolsson/tui-go v0.4.0
--- irc_client.go
+++ irc_client.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"strconv"
 	"net"
-	"time"
 	"bufio"
 	"strings"
 	"io"
+	"github.com/marcusolsson/tui-go"
 )
 
 type IRCClient struct {
@@ -18,20 +18,27 @@ type IRCClient struct {
 	addr string
 	port int
 	socket net.Conn
+	IRCTui
 }
 
-func (c *IRCClient) register_user() {
-	fmt.Fprintf(c.socket, "PASS " + c.pass + "\r\nUSER " + c.user + " * * :" + c.user + "\r\nNICK " + c.nick + "\r\n")
+type IRCTui struct {
+	uiChannel chan string
+	sidebar *tui.Box
+	history *tui.Box
 }
 
-func (c *IRCClient) receive_line() {
+func (c *IRCClient) send(data string) {
+	fmt.Fprintf(c.socket, data)
+}
+
+func (c *IRCClient) receive() {
 	for {
 		reader := bufio.NewReader(c.socket) // Probably not the best, should probably move into struct. I feel like there's a more elegant solution though.
 		ba, _, err := reader.ReadLine()
 		if err != nil && err != io.EOF {
 			fmt.Println("Server closed connection.")
 			c.socket.Close()
-			os.Exit(0)	
+			break
 		}
 
 		line := string(ba)
@@ -39,9 +46,12 @@ func (c *IRCClient) receive_line() {
 		if strings.Contains(line, "PING") {
 			fmt.Fprintf(c.socket, "PONG :" + strings.Split(line, ":")[1] + "\r\n")
 		} else {
-			println(line)
+			c.uiChannel <- line
+			//fmt.Println(line)
 		}
 	}
+
+	close(c.uiChannel)
 }
 
 func (c *IRCClient) dump_buf() {
@@ -59,37 +69,100 @@ func (c *IRCClient) dump_buf() {
 	}
 }
 
-func initiate_connection(addr string, port int, user string, nick string, pass string) {
-	full_addr := addr + ":" + strconv.Itoa(port)
+func (c *IRCClient) register_user() {
+	c.send("PASS " + c.pass + "\r\nUSER " + c.user + " * * :" + c.user + "\r\nNICK " + c.nick + "\r\n")
+}
+
+func (c *IRCClient) initiate_connection() {
+	full_addr := c.addr + ":" + strconv.Itoa(c.port)
 	fmt.Print("Initiating connection with " + full_addr + "...")
 	conn, err := net.Dial("tcp", full_addr)
-	
-	if(err != nil) {
-		fmt.Println("Couldn't initiate connection with " + full_addr)
-		return
+	if(err != nil || conn == nil) {
+		fmt.Println("Timed out")
+		os.Exit(0)
 	}
-	
+	c.socket = conn
 	fmt.Println("Success")
-	client := &IRCClient { addr: addr, port: port, socket: conn, user: user, nick: nick, pass: pass, }
-	client.register_user()
-	go client.receive_line()
-	
-	time.Sleep(20 * time.Second)
+}
+
+func (t *IRCClient) build_ui() {
+	t.sidebar = tui.NewVBox(tui.NewLabel("CHANNELS"))
+	t.sidebar.SetBorder(true)
+	t.history = tui.NewVBox()
+	historyScroll := tui.NewScrollArea(t.history)
+	historyScroll.SetAutoscrollToBottom(true)
+	historyBox := tui.NewVBox(historyScroll)
+	historyBox.SetBorder(true)
+	input := tui.NewEntry()
+	input.SetFocused(true)
+	input.SetSizePolicy(tui.Expanding, tui.Maximum)
+	inputBox := tui.NewHBox(input)
+	inputBox.SetBorder(true)
+	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	chat := tui.NewVBox(historyBox, inputBox)
+	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
+	root := tui.NewHBox(t.sidebar, chat)
+	ui, _ := tui.New(root)
+
+	ui.SetKeybinding("Esc", func() { 
+		ui.Quit()
+		close(t.uiChannel)
+	})
+
+	input.OnSubmit(func(e *tui.Entry) {
+		t.send(e.Text() + "\r\n")
+		t.history.Append(tui.NewHBox(tui.NewLabel(e.Text())))
+		input.SetText("")
+	})
+
+	go func() {
+		if err := ui.Run(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for {
+		val, ok := <-t.uiChannel
+		if !ok {
+			break
+		}
+		ui.Update(func() {
+			t.history.Append(tui.NewHBox(tui.NewLabel(val)))
+		})
+	}
 }
 
 func main() {
 	args := os.Args[1:]
-	
+
 	if len(args) != 5 && len(args) != 4 {
 		fmt.Println("Usage: irc_client [username] [nick] [pass] [address] [port (default = 6667)]")
-	} else if len(args) == 4 {
-		initiate_connection(args[3], 6667, args[0], args[1], args[2]);
 	} else {
-		port, err := strconv.Atoi(args[4])
-		if err == nil { 
-			initiate_connection(args[3], port, args[0], args[1], args[2])
-		} else {
-			fmt.Println("Invalid port number. Enter a port between 0 and 65535.")
+		var port int
+		if len(args) == 4 { 
+			port = 6667 
+		} else { 
+			p, err := strconv.Atoi(args[4])
+			if err == nil { 
+				port = p
+			} else {
+				fmt.Println("Invalid port number. Enter a port between 0 and 65535.")
+				os.Exit(0)
+			}
 		}
+
+		uiChannel := make(chan string)
+		client := new(IRCClient)
+		client.addr = args[3]
+		client.port = port
+		client.user = args[0]
+		client.nick = args[1]
+		client.pass = args[2]
+		client.uiChannel = uiChannel
+	
+		client.initiate_connection()
+		client.register_user()
+		go client.receive()		
+		client.build_ui()
 	}
 }
